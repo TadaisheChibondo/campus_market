68d4dd7aeaf1eab4471933fb170273aa1f4f02e5
Tadaishe Chibondo <tadaishechibondo@gmail.com>
2026-01-08 16:18:03 +0200

    Fix CORS allowed origin

--- backend/settings.py
+++ backend/settings.py
@@ -140,7 +140,7 @@ if not DEBUG:
 
 # Configure Access & Media 
 CORS_ALLOWED_ORIGINS = [
-    "https://market-psi.vercel.app",  # <--- Changed from 3000 to 5173
+    "https://campus-market-psi.vercel.app", # <--- Changed from 3000 to 5173
 ]
 
 # Setting for uploading images
