b31c23092bb822791ed2825bed7bba801f848c57
Tadaishe Chibondo <tadaishechibondo@gmail.com>
2026-01-08 20:50:46 +0200

    Register Product model in admin

--- market/admin.py
+++ market/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Product
 
+# This tells the Admin panel: "Please let me manage Products here!"
+admin.site.register(Product)
 # Register your models here.
